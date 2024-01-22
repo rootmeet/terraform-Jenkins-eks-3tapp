5e0a4f0bfee3c5e934525111c204b422f675b270
Root Meet <rootmeet@gmail.com>
2024-01-21 17:38:37 -0700

    git ignore updated

--- Jenkins-Server/jenkins-install.sh
+++ Jenkins-Server/jenkins-install.sh
@@ -11,6 +11,11 @@ sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
+# Install latest aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
 # then install git
 sudo yum install git -y
 
